539cd20d460088a0a633832b7652385ebd114fcb
DiegoAleksis <diegoarh19@gmail.com>
2022-03-03 11:59:07 -0600

    Avance de gett y sett

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,2 +1,18 @@
 public class Circulo {
+float radio;
+
+public Circulo(){
+    System.out.println("Hola miss, soy el constructor por defecto y no sirvo para nada");}
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public Circulo(float radio) {
+        this.radio = radio;
+    }
 }
